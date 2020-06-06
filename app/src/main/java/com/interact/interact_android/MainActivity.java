27ceebcc82a4f66e5a80822e44eec15f712effeb
ethan-bit <ethan-bit@protonmail.com>
2020-06-07 00:25:58 +0800

    Can be scanned and can be changed

--- app/src/main/java/com/interact/interact_android/MainActivity.java
+++ app/src/main/java/com/interact/interact_android/MainActivity.java
@@ -4,16 +4,19 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.interact.interact_android.android.CaptureActivity;
+import com.interact.interact_android.home.HomeFragment;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -25,13 +28,22 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private static final String DECODED_CONTENT_KEY = "codedContent";
+//    private static final String DECODED_BITMAP_KEY = "codedBitmap";
     private static final int REQUEST_CODE_SCAN = 0x0000;
 
+    private TextView tv_scanResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        View fragment_homeview=this.getLayoutInflater().inflate(R.layout.fragment_home, null);
+//        tv_scanResult = fragment_homeview.findViewById(R.id.text_home);
+//        View root = inflater.inflate(R.layout.fragment_home, container, false);
+//        final TextView textView = root.findViewById(R.id.text_home);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -57,6 +69,16 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+//        View inflate = this.getLayoutInflater().inflate(R.layout.fragment_home, navigationView.get, false);
+//
+//        final TextView textView = root.findViewById(R.id.text_home);
+//
+//        Fragment fragmentById = drawer.getfr(R.id.nav_home);
+//        System.out.println("");
+//        tv_scanResult =  navigationView.getfindViewById(R.id.text_home);
+//        tv_scanResult = navController.getNavInflater().(R.id.text_home);
+//        final TextView textView = root.findViewById(R.id.text_home);
     }
 
     /**
@@ -79,5 +101,22 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 扫描二维码/条码回传
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) {
+                //返回的文本内容
+                String content = data.getStringExtra(DECODED_CONTENT_KEY);
+                //返回的BitMap图像
+//                Bitmap bitmap = data.getParcelableExtra(DECODED_BITMAP_KEY);
+                /*取出Intent中附加的数据*/
+
+                HomeFragment.setText("你扫描到的内容是：" + content);
+            }
+        }
     }
 }
--- app/src/main/java/com/interact/interact_android/home/HomeFragment.java
+++ app/src/main/java/com/interact/interact_android/home/HomeFragment.java
@@ -17,13 +17,13 @@ import com.interact.interact_android.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    static TextView textView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -31,5 +31,9 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    public static void setText(String txt){
+        textView.setText(txt);
     }
 }
